403364c51a6301a04f45126efd3ca2de75de694e
Teodor Wojcik <teodor.wojcik@gmail.com>
2017-04-22 19:13:47 +0200

    nice-format

--- videochatroom.js
+++ videochatroom.js
@@ -2,6 +2,32 @@
 // Compatibility shim
 navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
  
+function pretty_time_string(num) {
+    return ( num < 10 ? "0" : "" ) + num;
+  }
+
+var start = new Date;    
+
+setInterval(function() {
+  var total_seconds = (new Date - start) / 1000;   
+
+  var hours = Math.floor(total_seconds / 3600);
+  total_seconds = total_seconds % 3600;
+
+  var minutes = Math.floor(total_seconds / 60);
+  total_seconds = total_seconds % 60;
+
+  var seconds = Math.floor(total_seconds);
+
+  hours = pretty_time_string(hours);
+  minutes = pretty_time_string(minutes);
+  seconds = pretty_time_string(seconds);
+
+  var currentTimeString = minutes + ":" + seconds;
+
+  $('.Timer').text(currentTimeString);
+}, 1000);
+
 var peer;
 function makePeer(id_number){
 
@@ -54,7 +80,7 @@ for (var i = 1; i< peer.id; i++)
 function getLocalVideo() {
   navigator.getUserMedia({audio: true, video: true}, function(stream){
     console.log("Local video streaming");
-    $('#videos').append("<video id='" + peer.id + "' autoplay></video>");
+    $('#videos').append("<div class='col-s12 center-align'><video id='" + peer.id + "' autoplay></video></div>");
     $('#' + peer.id).prop('src', URL.createObjectURL(stream));
     window.localStream = stream;
     getPreviousStreams();
@@ -83,7 +109,7 @@ function processCall(call) {
   // Wait for stream on the call, then set peer video display
   call.on('stream', function(remoteStream){
     console.log("Adding video from " + call.peer);
-    $('#videos').append("<video id='" + call.peer + "' autoplay>");
+    $('#videos').append("<div class='col-s12 center-align'><video id='" + call.peer + "' autoplay></div>");
     $('#' + call.peer).prop('src', URL.createObjectURL(remoteStream));
   });
  
